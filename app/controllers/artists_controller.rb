53257bd60f4f19c8f9a58032674d8df83c460597
clberry7107 <clberry7107@yahoo.com>
2015-11-25 20:20:15 +0000

    sort not functioning in PG, commit for production enviroment

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -7,7 +7,7 @@ class ArtistsController < ApplicationController
 	
 	def index
 		#Show all saved artists
-		@artists = Artist.all.order(sort_column + " " + sort_direction)
+		@artists = Artist.all.order("ListName")
 	end
 
 	def new
