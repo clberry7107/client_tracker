aefcd1e606934553d0fb2fa6b6924547da728e4f
clberry7107 <clberry7107@yahoo.com>
2016-09-19 15:34:17 +0000

    adding symbols to cal via region, not city

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-    
+    shops = ["Clevend", "Los Angeles", "London", "Sydney"]
     def sortable(column, title=nil)
         title ||= column.titleize
         direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
@@ -9,6 +9,33 @@ module ApplicationHelper
         link_to title, :sort => column, :direction => direction, :secondary => second_sort
     end
     
-
+    def near_shop(cities)
+        markers = Array.new
+        cities.each do |city|
+            markers << shop_marker(city.Region)
+        end
+        markers.reject!{|item| item.empty?}
+        marks = ""
+        markers.each do |mark|
+            marks << mark
+        end
+        return marks
+    end
+    
+    def shop_marker(region)
+        add = ""
+        case region
+        when "Cleveland, OH"
+            add << "*"
+        when "Los Angeles, CA"
+            add << "<"
+        when "London, United Kingdom"
+           add << "~"
+        when "Sydney, Austrailia"
+            add << "v"
+        end
+           
+        return add   
+    end
 
 end
